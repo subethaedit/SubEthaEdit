c4cf362b9f764fbc71eb04f3e3034e49d4260444
Dominik Wagner <dom@codingmonkeys.de>
2003-09-17 16:08:04 +0000

    some first interface declaration

--- prototype/TCMXMLParser/TCMXMLParser.h
+++ prototype/TCMXMLParser/TCMXMLParser.h
@@ -1,16 +1,51 @@
 //
-//  TCMXLMParser.h
+//  TCMXMLParser.h
 //  TCMXMLParser
 //
 //  Created by Dominik Wagner on Wed Sep 17 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2003 TheCodingMonkeys. All rights reserved.
 //
+//  expat reference: http://www.xml.com/lpt/a/1999/09/expat/reference.html
+//  expat tutorial:  http://www.xml.com/lpt/a/1999/09/expat/index.html
 
 #import <Foundation/Foundation.h>
-
+#import "expat.h"
+/*!
+    @class      TCMXMLParser
+    @abstract   A stream oriented XML parser
+    @discussion This class provides a XML parser that is able to parse XML Streams, like used in e.g. Jabber/XMPP.
+*/
 
 @interface TCMXMLParser : NSObject {
-
+    XML_Parser CM_expatParser;
+    id         CM_delegate;
 }
+
++ (TCMXMLParser)XMLParser;
+
+- (id)init;
+
+- (void)setDelegate:(id)aDelegate;
+- (id)delegate;
+
+- (void)parseData:(NSData *)aData moreComing:(BOOL)moreComing;
+
+- (int)columnNumber;
+- (int)lineNumber;
+
+@end
+
+@interface NSObject (TCMXMLParserDelegate)
+
+- (void)parser:(TCMXMLParser *)parser didStartElement:(NSString *)elementName 
+        namespaceURI:(NSString *)namespaceURI attributes:(NSDictionary *)attributeDict;
+- (void)parser:(TCMXMLParser *)parser didEndElement:(NSString *)elementName 
+        namespaceURI:(NSString *)namespaceURI;
+- (void)parser:(TCMXMLParser *)parser didStartMappingPrefix:(NSString *)prefix toURI:(NSString *)namespaceURI;
+- (void)parser:(TCMXMLParser *)parser didEndMappingPrefix:(NSString *)prefix;
+- (void)parser:(TCMXMLParser *)parser foundCharacters:(NSString *)string;
+- (void)parser:(TCMXMLParser *)parser foundCDATA:(NSData *)CDATABlock;
+- (void)parser:(TCMXMLParser *)parser foundComment:(NSString *)comment;
+- (void)parser:(TCMXMLParser *)parser foundProcessingInstructionWithTarget:(NSString *)target data:(NSString *)data;
 
 @end
--- prototype/TCMXMLParser/TCMXMLParser.m
+++ prototype/TCMXMLParser/TCMXMLParser.m
@@ -3,12 +3,17 @@
 //  TCMXMLParser
 //
 //  Created by Dominik Wagner on Wed Sep 17 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2003 TheCodingMonkeys. All rights reserved.
 //
 
 #import "TCMXMLParser.h"
 #import "expat.h"
 
 @implementation TCMXLMParser
+
+- (void)parseData:(NSData *)aData moreComing:(BOOL)moreComing
+{
+    // int XML_Parse(XML_Parser p, const char *s, int len, int isFinal)
+}
 
 @end
