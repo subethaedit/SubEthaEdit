e4ed166b6044abebbea6d0e04e48c2ac9a8b9ec1
Martin Ott <mbo@codingmonkeys.de>
2004-02-24 20:47:41 +0000

    - Added initial SubEthaEdit project

---
+++ subethaedit/MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Tue Feb 24 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ subethaedit/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Tue Feb 24 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
