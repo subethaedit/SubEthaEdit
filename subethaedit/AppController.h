e0263e666d4c43dc942578bc51698773d44c036f
Martin Ott <mbo@codingmonkeys.de>
2004-02-25 13:03:58 +0000

    - Added AppController - Extended copyright duration in InfoPlist.strings - Renamed 'NewApplication' to 'SubEthaEdit' in MainMenu

---
+++ subethaedit/AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Wed Feb 25 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ subethaedit/AppController.m
@@ -0,0 +1,14 @@
+//
+//  AppController.m
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Wed Feb 25 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+@end
