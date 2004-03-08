8c4834c28197ca50e33163cb6a7583549215778b
Martin Ott <mbo@codingmonkeys.de>
2004-03-08 18:15:06 +0000

    - Added TCMMMBrowserListView skeleton files

---
+++ subethaedit/TCMMillionMonkeys/TCMMMBrowserListView.h
@@ -0,0 +1,16 @@
+//
+//  TCMMMBrowserListView.h
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Mon Mar 08 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+@interface TCMMMBrowserListView : NSView {
+
+}
+
+@end
---
+++ subethaedit/TCMMillionMonkeys/TCMMMBrowserListView.m
@@ -0,0 +1,26 @@
+//
+//  TCMMMBrowserListView.m
+//  SubEthaEdit
+//
+//  Created by Martin Ott on Mon Mar 08 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import "TCMMMBrowserListView.h"
+
+
+@implementation TCMMMBrowserListView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)rect {
+    // Drawing code here.
+}
+
+@end
