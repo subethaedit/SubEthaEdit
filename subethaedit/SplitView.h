b7cb38dc95fbc2cbb1ca3a42b98667f7af824c92
Dominik Wagner <dom@codingmonkeys.de>
2004-04-13 13:50:38 +0000

    - now with Splitview that paints its background with   "[NSColor _toolbarBackgroundColor]" and has a fallback   that does almost the same if the privat method vanishes...

---
+++ subethaedit/SplitView.h
@@ -0,0 +1,16 @@
+//
+//  SplitView.h
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on Tue Apr 13 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+@interface SplitView : NSSplitView {
+
+}
+
+@end
---
+++ subethaedit/SplitView.m
@@ -0,0 +1,42 @@
+//
+//  SplitView.m
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on Tue Apr 13 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import "SplitView.h"
+
+
+@interface NSColor (AppleInternalAdditions)
+
++ (NSColor *) _toolbarBackgroundColor;
+
+@end
+
+@implementation SplitView
+
+- (float)dividerThickness {
+    return [super dividerThickness]+6.;
+}
+
+- (void)drawDividerInRect:(NSRect)aRect {
+    static NSColor *color;
+    if (!color) {
+        if ([NSColor respondsToSelector:@selector(_toolbarBackgroundColor)]) {
+            color = [[NSColor _toolbarBackgroundColor] retain];
+        } else {
+            color = [[[NSColor whiteColor] colorWithAlphaComponent:0.4] retain];
+        }
+    }
+    [color set];
+    [[NSBezierPath bezierPathWithRect:aRect] fill];
+    [[NSColor grayColor] set];
+    aRect.origin.x-=1;
+    aRect.size.width+=2;
+    NSFrameRect(aRect);
+    [super drawDividerInRect:aRect];
+}
+
+@end
