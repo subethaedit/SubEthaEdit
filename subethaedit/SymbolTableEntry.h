e6e1959cf2390c13c45a5da9b38f5ebb2745fb00
Dominik Wagner <dom@codingmonkeys.de>
2004-04-16 13:23:22 +0000

    - added SymbolTableEntry

---
+++ subethaedit/SymbolTableEntry.h
@@ -0,0 +1,35 @@
+//
+//  SymbolTableEntry.h
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on Fri Apr 16 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SymbolTableEntry : NSObject {
+    NSString *I_name;
+    int       I_fontTraitMask;
+    NSImage  *I_image;
+    NSString *I_type;
+    NSRange  I_jumpRange;
+    NSRange  I_range;
+}
+
++ symbolTableEntryWithName:(NSString *)aName fontTraitMask:(int)aMask image:(NSImage *)anImage type:(NSString *)aType jumpRange:(NSRange)aJumpRange range:(NSRange)aRange;
+
+- (NSString *)name;
+- (void)setName:(NSString *)name;
+- (int)fontTraitMask;
+- (void)setFontTraitMask:(int)aMask;
+- (NSImage *)image;
+- (void)setImage:(NSImage *)anImage;
+- (NSString *)type;
+- (void)setType:(NSString *)aType;
+- (NSRange)jumpRange;
+- (void)setJumpRange:(NSRange)aJumpRange;
+- (NSRange)range;
+- (void)setRange:(NSRange)aRange;
+@end
---
+++ subethaedit/SymbolTableEntry.m
@@ -0,0 +1,72 @@
+//
+//  SymbolTableEntry.m
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on Fri Apr 16 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import "SymbolTableEntry.h"
+
+
+@implementation SymbolTableEntry
++ symbolTableEntryWithName:(NSString *)aName fontTraitMask:(int)aMask image:(NSImage *)anImage type:(NSString *)aType jumpRange:(NSRange)aJumpRange range:(NSRange)aRange {
+    SymbolTableEntry *result=[[SymbolTableEntry new] autorelease];
+    [result setName:aName];
+    [result setFontTraitMask:aMask];
+    [result setImage:anImage];
+    [result setType:aType];
+    [result setJumpRange:aJumpRange];
+    [result setRange:aRange];
+    return result;
+}
+
+- (void)dealloc {
+    [I_name release];
+    [I_image release];
+    [I_type release];
+    [super dealloc];
+}
+
+- (NSString *)name {
+    return I_name;
+}
+- (void)setName:(NSString *)aName {
+    [I_name autorelease];
+    I_name = [aName copy];
+}
+- (int)fontTraitMask {
+    return I_fontTraitMask;
+}
+- (void)setFontTraitMask:(int)aMask {
+    I_fontTraitMask = aMask;
+}
+- (NSImage *)image {
+    return I_image;
+}
+- (void)setImage:(NSImage *)anImage {
+    [I_image autorelease];
+    I_image = [anImage retain];
+}
+
+- (NSString *)type {
+    return I_type;
+}
+- (void)setType:(NSString *)aType {
+    [I_type autorelease];
+    I_type = [aType copy];
+}
+- (NSRange)jumpRange {
+    return I_jumpRange;
+}
+- (void)setJumpRange:(NSRange)aJumpRange {
+    I_jumpRange=aJumpRange;
+}
+- (NSRange)range {
+    return I_range;
+}
+- (void)setRange:(NSRange)aRange {
+    I_range=aRange;
+}
+
+@end
