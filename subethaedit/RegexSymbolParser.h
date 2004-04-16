b093118a221841615f1bf7cccd56fe623227f16d
Martin Pittenauer <map@codingmonkeys.de>
2004-04-16 13:39:35 +0000

    - adding SymbolParser stub

---
+++ subethaedit/RegexSymbolParser.h
@@ -0,0 +1,20 @@
+//
+//  RegexSymbolParser.h
+//  SubEthaEdit
+//
+//  Created by Martin Pittenauer on Fri Apr 16 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RegexSymbolParser : NSObject {
+
+}
+
+/*"Document Interaction"*/
+- (NSArray *)symbolsForTextStorage:(NSTextStorage *)aTextStorage;
+
+
+@end
---
+++ subethaedit/RegexSymbolParser.m
@@ -0,0 +1,28 @@
+//
+//  RegexSymbolParser.m
+//  SubEthaEdit
+//
+//  Created by Martin Pittenauer on Fri Apr 16 2004.
+//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//
+
+#import "RegexSymbolParser.h"
+#import "SymbolTableEntry.h"
+
+@implementation RegexSymbolParser
+
+- (id)init 
+{
+    self=[super init];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (NSArray *)symbolsForTextStorage:(NSTextStorage *)aTextStorage 
+{
+    return nil;
+}
+
+@end
