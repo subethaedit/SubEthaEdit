ddb5509b53b6d85456beaf4e03d0e71ff4eed706
Martin Ott <mbo@codingmonkeys.de>
2004-10-26 10:49:19 +0000

    Removed obsolete script. Instead of setting the ownership to root:admin the script now only sets the group to admin. This allows creating dist images as admin user.

--- createDiskImage.sh
+++ createDiskImage.sh
@@ -33,7 +33,7 @@ find "${BUILT_PRODUCTS_DIR}/${DiskImageProduct}" -name ".svn" -exec rm -rvf "{}"
 echo "...done"
 echo
 echo "Setting ownership..."
-/usr/sbin/chown -R root:admin "${BUILT_PRODUCTS_DIR}/${DiskImageProduct}"
+/usr/sbin/chgrp -R admin "${BUILT_PRODUCTS_DIR}/${DiskImageProduct}"
 echo "...done"
 echo "Setting permissions..."
 /bin/chmod -R g+w "${BUILT_PRODUCTS_DIR}/${DiskImageProduct}"
@@ -83,65 +83,3 @@ echo
 osascript -e "tell application \"Finder\"" -e "select posix file \"${TARGET_BUILD_DIR}/${DiskImageVolumeName}.dmg\"" -e "end tell" > /dev/null
 
 exit 0
-
-# #! /bin/sh
-# #
-# # createDiskImage
-# 
-# # Requires three or more args
-# if [ $# -lt 3 ] ; then
-#     echo "usage: $0 <ImageSizeInMegabytes> <NameOfImageWithNoExtension> <FolderToCopyToImage> ..."
-#     exit 1
-# fi
-# 
-# # Grab the size and image name arguments.  Leave the rest of them in $*
-# imageSize=$1
-# shift
-# imageName=$1
-# shift
-# 
-# # Create the image and format it
-# echo
-# echo "Creating ${imageSize} MB disk image named ${imageName}..."
-# 
-# rm -f ${imageName}.dmg
-# hdiutil create ${imageName}.dmg -megabytes ${imageSize} -layout NONE
-# 
-# hdidOutput=`hdid -nomount ${imageName}.dmg | grep '/dev/disk[0-9]*' | awk '{print $1}'`
-# 
-# /sbin/newfs_hfs -w -v ${imageName} -b 4096  ${hdidOutput}
-# 
-# hdiutil eject ${hdidOutput}
-# 
-# # Mount the image and copy stuff
-# hdidOutput=`hdid ${imageName}.dmg | grep '/dev/disk[0-9]*' | awk '{print $1}'`
-# sleep 4 # This sleep is needed and there seems to be no way to know when the image is mounted...
-# 
-# echo "Copying contents to ${imageName}..."
-# while [ $# -gt 0 ] ; do
-#     echo "...cleaning ${1}"
-#     find ${1} -name ".svn" -exec rm -rf "{}" \;
-#     echo "...setting ownership"
-#     /usr/sbin/chown -R root:admin ${1}
-#     echo "...setting permissions"
-#     /bin/chmod -R g+w ${1}
-#     echo "...copying ${1}"
-#     /Developer/Tools/CpMac -r ${1} /Volumes/${imageName}
-#     shift
-# done
-# 
-# hdiutil eject ${hdidOutput}
-# 
-# # Compress the image
-# echo "Compressing ${imageName} disk image..."
-# 
-# mv ${imageName}.dmg ${imageName}.orig.dmg
-# hdiutil convert ${imageName}.orig.dmg -format UDCO -o ${imageName}
-# rm ${imageName}.orig.dmg
-# 
-# # Internet-enable the image
-# echo "Internet-enabling ${imageName} disk image..."
-# hdiutil internet-enable -yes ${imageName}.dmg
-# 
-# echo "Done."
-# echo
