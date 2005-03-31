b441f8672fa3d50cdef97136c7d162d970552701
Martin Ott <mbo@codingmonkeys.de>
2005-03-31 15:34:45 +0000

    Added TCMFoundation framework.

---
+++ subethaedit/TCMFoundation/TCMFoundation.h
@@ -0,0 +1,10 @@
+//
+//  TCMFoundation.h
+//  TCMFoundation
+//
+//  Created by Martin Ott on Thu Mar 31 2005.
+//  Copyright (c) 2005 TheCodingMonkeys. All rights reserved.
+//
+
+#import <TCMFoundation/NSDictionaryTCMAdditions.h>
+#import <TCMFoundation/NSStringTCMAdditions.h>
