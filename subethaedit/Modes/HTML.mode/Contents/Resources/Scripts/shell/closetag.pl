4624107cc9e74217e3dd1bc6083169d8d9a6e0e4
Martin Pittenauer <map@codingmonkeys.de>
2006-05-23 12:33:24 +0000

    Fixing close last tag again.

--- subethaedit/Modes/HTML.mode/Contents/Resources/Scripts/shell/closetag.pl
+++ subethaedit/Modes/HTML.mode/Contents/Resources/Scripts/shell/closetag.pl
@@ -3,11 +3,11 @@ use strict;
 
 my $data = join "",<STDIN>;
 $data =~ s/[\n\r]/ /g;          # throw out line breaks
-$data =~ s/\A[^<]//g;           # filter all characters up to first tag
+$data =~ s/\A[^<]*//g;          # filter all characters up to first tag
 $data =~ s/<!--.*?-->//g;       # strip comments
 $data =~ s/(?<=>)[^<]*//g;      # remove everthing that's not a tag
 $data =~ s/ .*?(\/?) *>/\1>/g;  # remove attributes
-$data =~ s/\A[^<]*<//g;         # remove everything up to the first <
+$data =~ s/\A<//g;              # remove everything up to the first <
 $data =~ s/>[^>]*\Z//g;         # remove eyerthing after the last >
 $data =~ s/ //g;
 
