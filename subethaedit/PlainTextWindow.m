e4de76451c9d487251d7f5851861a6cc9f748e98
Martin Ott <mbo@codingmonkeys.de>
2006-11-23 10:43:15 +0000

    Fixed [SEE-1175]: Close command doesn't close about window
    Subclassed NSWindow to catch the -performClose: action. Subclassed NSWindow
    is used for PlainTextWindows which redirect the action to -closeTab:.

---
+++ subethaedit/PlainTextWindow.m
@@ -0,0 +1,24 @@
+//
+//  PlainTextWindow.m
+//  SubEthaEdit
+//
+//  Created by Martin Ott on 11/23/06.
+//  Copyright 2006 TheCodingMonkeys. All rights reserved.
+//
+
+#import "PlainTextWindow.h"
+#import "PlainTextWindowController.h"
+
+
+@implementation PlainTextWindow
+
+- (IBAction)performClose:(id)sender
+{
+    if ([[self windowController] isKindOfClass:[PlainTextWindowController class]]) {
+        [(PlainTextWindowController *)[self windowController] closeTab:sender];
+    } else {
+        [super performClose:sender];
+    }
+}
+
+@end
