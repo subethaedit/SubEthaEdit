48f7cc70a6724066b167d00965bf3e010a01d4cc
Martin Ott <mbo@codingmonkeys.de>
2006-12-20 12:58:57 +0000

    Added PSMImageView.

---
+++ subethaedit/PSMTabBarControl/PSMImageView.h
@@ -0,0 +1,16 @@
+//
+//  PSMImageView.h
+//  PSMTabBarControl
+//
+//  Created by Martin Ott on 12/19/06.
+//  Copyright 2006 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PSMImageView : NSView {
+    NSImage *_image;
+}
+
+@end
