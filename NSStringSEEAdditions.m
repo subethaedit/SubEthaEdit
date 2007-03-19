46776a701789aa044154014e4fbdf6f42fc16b76
Martin Ott <mbo@codingmonkeys.de>
2007-03-19 16:11:40 +0000

    Split up NSStringSEEAdditions and TCMMMUserManager to honor AppKit dependencies.

--- NSStringSEEAdditions.m
+++ NSStringSEEAdditions.m
@@ -3,11 +3,10 @@
 //  
 //
 //  Created by Martin Ott on Tue Feb 17 2004.
-//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//  Copyright (c) 2004-2007 TheCodingMonkeys. All rights reserved.
 //
 
 #import "NSStringSEEAdditions.h"
-#import <OgreKit/OgreKit.h>
 
 
 static void convertLineEndingsInString(NSMutableString *string, NSString *newLineEnding)
@@ -699,114 +698,6 @@ static void convertLineEndingsInString(NSMutableString *string, NSString *newLin
     [stateStack release];
     [state release];
     return result;
-}
-
-@end
-
-
-
-@implementation NSMutableAttributedString (NSMutableAttributedStringSEEAdditions) 
-
-- (NSRange)detab:(BOOL)shouldDetab inRange:(NSRange)aRange tabWidth:(int)aTabWidth askingTextView:(NSTextView *)aTextView {
-    [self beginEditing];
-
-    static OGRegularExpression *tabExpression,*spaceExpression;
-    if (!tabExpression) {
-        tabExpression  =[[OGRegularExpression regularExpressionWithString:@"\t+"] retain];
-        spaceExpression=[[OGRegularExpression regularExpressionWithString:@"  +"] retain];
-    }
-
-    unsigned changeInLength=0;
-    
-    if (shouldDetab) {
-        NSArray *matches=[tabExpression allMatchesInString:[self string] range:aRange];
-        int i=0;
-        int count=[matches count];
-        for (i=0;i<count;i++) {
-            OGRegularExpressionMatch *match=[matches objectAtIndex:i];
-            NSRange matchRange=[match rangeOfMatchedString];
-            matchRange.location+=changeInLength;
-            NSRange lineRange=[[self string] lineRangeForRange:matchRange];
-            int replacementStringLength=(matchRange.length-1)*aTabWidth+
-                                        (aTabWidth-(matchRange.location-lineRange.location)%aTabWidth);
-            NSString *replacementString=[@"" stringByPaddingToLength:replacementStringLength withString:@" " startingAtIndex:0];
-            if ((aTextView && [aTextView shouldChangeTextInRange:matchRange replacementString:replacementString]) 
-                || !aTextView) {
-                [self replaceCharactersInRange:matchRange withString:replacementString];
-                if (aTextView) {
-                    [self addAttributes:[aTextView typingAttributes] 
-                          range:NSMakeRange(matchRange.location,replacementStringLength)];
-                }
-                changeInLength+=replacementStringLength-matchRange.length;
-            }
-        }
-    } else {
-        NSArray *matches=[spaceExpression allMatchesInString:[self string] range:aRange];
-        int i=0;
-        int count=[matches count];
-        for (i=count-1;i>=0;i--) {
-            OGRegularExpressionMatch *match=[matches objectAtIndex:i];
-            NSRange matchRange=[match rangeOfMatchedString];
-            NSRange lineRange=[[self string] lineRangeForRange:matchRange];
-            if (matchRange.length>=(aTabWidth-(matchRange.location-lineRange.location)%aTabWidth)) {
-                // align end of spaces to tab boundary
-                matchRange.length-=(NSMaxRange(matchRange)-lineRange.location)%aTabWidth;
-                int replacementStringLength=matchRange.length/aTabWidth;
-                if ((matchRange.location-lineRange.location)%aTabWidth!=0) replacementStringLength+=1;
-                NSString *replacementString=[@"" stringByPaddingToLength:replacementStringLength withString:@"\t" startingAtIndex:0];
-                if ((aTextView && [aTextView shouldChangeTextInRange:matchRange replacementString:replacementString])
-                    || !aTextView) {
-                    [self replaceCharactersInRange:matchRange withString:replacementString];
-                    if (aTextView) {
-                        [self addAttributes:[aTextView typingAttributes] 
-                              range:NSMakeRange(matchRange.location,replacementStringLength)];
-                    }
-                    changeInLength+=replacementStringLength-matchRange.length;
-                }
-            }
-        }
-    }
-
-    aRange.length+=changeInLength;
-    [self endEditing];
-
-    [aTextView didChangeText];
-    return aRange;
-}
-
-
-- (void)makeLeadingWhitespaceNonBreaking {
-    NSString *hardspaceString=nil;
-    if (hardspaceString==nil) {
-        unichar hardspace=0x00A0;
-        hardspaceString=[[NSString stringWithCharacters:&hardspace length:1] retain];
-    }
-    unsigned index=[self length];
-    unsigned startIndex,lineEndIndex,contentsEndIndex;
-    while (index!=0) {
-        [[self string] getLineStart:&startIndex end:&lineEndIndex contentsEnd:&contentsEndIndex forRange:NSMakeRange(index-1,0)];
-        unsigned firstNonWhitespace=startIndex;
-        NSString *string=[self string];
-        while (firstNonWhitespace<contentsEndIndex &&
-               [string characterAtIndex:firstNonWhitespace]==' ') {
-            firstNonWhitespace++;
-        }
-        if (firstNonWhitespace>startIndex) {
-            NSRange replaceRange=NSMakeRange(startIndex,firstNonWhitespace-startIndex);
-            [self replaceCharactersInRange:replaceRange
-                  withString:[@"" stringByPaddingToLength:replaceRange.length withString:hardspaceString startingAtIndex:0]];
-        }
-        index=startIndex;
-    }
-    
-    OGRegularExpression *moreThanOneSpace=[[[OGRegularExpression alloc] initWithString:@"  +" options:OgreFindLongestOption|OgreFindNotEmptyOption] autorelease];
-    NSEnumerator *matches=[[moreThanOneSpace allMatchesInString:[self string] range:NSMakeRange(0,[self length])] reverseObjectEnumerator];
-    OGRegularExpressionMatch *match=nil;
-    while ((match=[matches nextObject])) {
-        NSRange matchRange=[match rangeOfMatchedString];
-        [self replaceCharactersInRange:matchRange
-              withString:[@" " stringByPaddingToLength:matchRange.length withString:hardspaceString startingAtIndex:0]];
-    }
 }
 
 @end
--- TCMMillionMonkeys/TCMMMUserManager.m
+++ TCMMillionMonkeys/TCMMMUserManager.m
@@ -3,7 +3,7 @@
 //  SubEthaEdit
 //
 //  Created by Dominik Wagner on Wed Feb 25 2004.
-//  Copyright (c) 2004 TheCodingMonkeys. All rights reserved.
+//  Copyright (c) 2004-2007 TheCodingMonkeys. All rights reserved.
 //
 
 #import "TCMMMUserManager.h"
@@ -168,80 +168,6 @@ static TCMMMUserManager *sharedInstance=nil;
 
 - (NSArray *)allUsers {
     return [I_usersByID allValues];
-}
-
-- (BOOL)validateMenuItem:(NSMenuItem *)anItem {
-    SEL selector = [anItem action];
-    
-    if (selector == @selector(sendEmail:)) {
-    
-        BOOL isValid = NO;
-        NSEnumerator *enumerator = [[anItem representedObject] objectEnumerator];
-        NSString *userID;
-        while ((userID = [enumerator nextObject])) {
-            TCMMMUser *user = [self userForUserID:userID];
-            if ([(NSString *)[[user properties] objectForKey:@"Email"] length] > 0) {
-                isValid = YES;
-            } else {
-                isValid = NO;
-                break;
-            }
-        }
-        return isValid;
-        
-    } else if (selector == @selector(initiateAIMChat:)) {
-
-        BOOL isValid = NO;
-        NSEnumerator *enumerator = [[anItem representedObject] objectEnumerator];
-        NSString *userID;
-        while ((userID = [enumerator nextObject])) {
-            TCMMMUser *user = [self userForUserID:userID];
-            if ([(NSString *)[[user properties] objectForKey:@"AIM"] length] > 0) {
-                if ([userID isEqualToString:[TCMMMUserManager myUserID]]) {
-                    isValid = NO;
-                    break;
-                } else {
-                    isValid = YES;
-                }
-            } else {
-                isValid = NO;
-                break;
-            }
-        }
-        return isValid;    
-    }
-    
-    return YES;
-}
-
-- (IBAction)sendEmail:(id)sender {
-    NSMutableString *URLString = [NSMutableString stringWithString:@"mailto:"];
-    NSEnumerator *enumerator = [[sender representedObject] objectEnumerator];
-    NSString *userID;
-    BOOL hasRecipient = NO;
-    while ((userID = [enumerator nextObject])) {
-        TCMMMUser *user = [self userForUserID:userID];
-        NSString *email = [[user properties] objectForKey:@"Email"];
-        [URLString appendFormat:@"%@,", email];
-        hasRecipient = YES;
-    }
-    
-    if (hasRecipient) {
-        [URLString deleteCharactersInRange:NSMakeRange([URLString length] - 1, 1)];
-        DEBUGLOG(@"MillionMonkeysLogDomain", DetailedLogLevel, @"URLString: %@", URLString);
-        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:URLString]];
-    }
-}
-
-- (IBAction)initiateAIMChat:(id)sender {
-    NSEnumerator *enumerator = [[sender representedObject] objectEnumerator];
-    NSString *userID;
-    while ((userID = [enumerator nextObject])) {
-        TCMMMUser *user = [self userForUserID:userID];
-        NSString *screenname = [[user properties] objectForKey:@"AIM"];
-        NSString *URLString = [NSString stringWithFormat:@"aim:goim?screenname=%@", screenname];
-        [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:URLString]];
-    }
 }
 
 @end
