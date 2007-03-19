fa75848d25d9557d4dcb9b2ced9816c0b7916ad8
Martin Ott <mbo@codingmonkeys.de>
2007-03-19 16:11:40 +0000

    Split up NSStringSEEAdditions and TCMMMUserManager to honor AppKit dependencies.

---
+++ TCMMMUserManagerKitAdditions.h
@@ -0,0 +1,19 @@
+//
+//  TCMMMUserManagerKitAdditions.h
+//  SubEthaEdit
+//
+//  Created by Martin Ott on 3/19/07.
+//  Copyright 2007 TheCodingMonkeys. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+#import "TCMMillionMonkeys/TCMMMUserManager.h"
+
+
+@interface TCMMMUserManager (TCMMMUserManagerKitAdditions)
+
+- (BOOL)validateMenuItem:(NSMenuItem *)anItem;
+- (IBAction)sendEmail:(id)sender;
+- (IBAction)initiateAIMChat:(id)sender;
+
+@end
