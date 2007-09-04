8d816f3d847746543f66c1d624c3fbab9c4b9691
Martin Pittenauer <map@codingmonkeys.de>
2007-09-04 14:20:34 +0000

    Blocking pirated serial. Fixing bug introduced in r2350 that did not invalid kSyntaxHighlightingIsCorrectAttributeName correctly.

--- NSStringSEEAdditions.m
+++ NSStringSEEAdditions.m
@@ -135,7 +135,7 @@ static void convertLineEndingsInString(NSMutableString *string, NSString *newLin
     
     // Pirated number (2.6.3): SEE-BW26-J17T-QQ3Y (#1395435/557970)
     // Pirated number (2.6.3): SEE-Z410-71AH-5P0S (#798516/1669500)
-
+	// Pirated number (2.6.4): SEE-V006-81P0-35F4 (#123/1451814)	
     
     static int calls = 0;
     NSArray *splitArray = [self componentsSeparatedByString:@"-"];
@@ -165,6 +165,7 @@ static void convertLineEndingsInString(NSMutableString *string, NSString *newLin
 
             // check for pirated number            
             if (((number==1500) && (rndnumber == 63000)) ||
+                ((number==123) && (rndnumber == 1451814)) ||
                 ((number==43400) && (rndnumber == 136374)) ||
                 ((number==1395435) && (rndnumber == 557970)) ||
                 ((number==798516) && (rndnumber == 1669500)) ||
