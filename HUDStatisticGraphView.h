6618e673e265dc950784a422478c9dc93feba15d
Dominik Wagner <dom@codingmonkeys.de>
2007-09-18 19:04:44 +0000

    - added HMBlkAppKit for the statistics HUD - changed the statisticswindow to reflect the measures and look in the mock up - added some random graph painting so the bottom part is mocked up with random data

---
+++ HUDStatisticGraphView.h
@@ -0,0 +1,16 @@
+//
+//  HUDStatisticGraphView.h
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on 18.09.07.
+//  Copyright 2007 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface HUDStatisticGraphView : NSView {
+
+}
+
+@end
