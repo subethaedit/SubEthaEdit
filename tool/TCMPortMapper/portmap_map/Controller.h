4f729640e3db991cdb3ec7340e73c08fbe3f1d91
Martin Pittenauer <map@codingmonkeys.de>
2008-02-05 15:43:34 +0000

    Adding port mappings works in map app.

--- tool/TCMPortMapper/portmap_map/Controller.h
+++ tool/TCMPortMapper/portmap_map/Controller.h
@@ -12,9 +12,13 @@
 @interface Controller : NSObject {
 	IBOutlet NSWindow *o_sheetView;
 	IBOutlet NSTextField *o_statusTextField;
+	IBOutlet NSTextField *o_internalPort;
+	IBOutlet NSTextField *o_externalPort;
+	IBOutlet NSArrayController *o_arrayController;
 	NSArray *portMappings;
 }
 
 - (IBAction) addPortMapping:(id)sender;
+- (IBAction) mapNewPort:(id)sender;
 
 @end
--- tool/TCMPortMapper/portmap_map/Controller.m
+++ tool/TCMPortMapper/portmap_map/Controller.m
@@ -13,20 +13,30 @@
 @implementation Controller
 
 - (void) awakeFromNib {
-	//[o_statusTextField setStringValue:[NSString stringWithFormat:@"Status: External IP is %@", [[TCMPortMapper sharedInstance] externalIPAddress]]];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(portMapperNotifications:) name:nil object:[TCMPortMapper sharedInstance]];
 	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(portMapperNotifications:) name:TCMPortMappingDidChangeMappingStatusNotification object:nil];
-	[[TCMPortMapper sharedInstance] start]; // Just a test
-	[[TCMPortMapper sharedInstance] addPortMapping:[TCMPortMapping portMappingWithPrivatePort:6942 desiredPublicPort:6942 userInfo:nil]];
+	[[TCMPortMapper sharedInstance] start]; 
+
 }
 
 - (IBAction) addPortMapping:(id)sender {
-	[NSApp beginSheet:o_sheetView modalForWindow:[NSApp mainWindow] modalDelegate:self didEndSelector:@selector(sheetDidEnd:returnCode:returnCode:contextInfo:) contextInfo:nil];
+	[NSApp beginSheet:o_sheetView modalForWindow:[NSApp mainWindow] modalDelegate:self didEndSelector:nil contextInfo:nil];
 }
 
-- (void)sheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo {
+- (void)mapNewPort:(id)sender {
+
+	int internal = [o_internalPort intValue];
+	int external = [o_externalPort intValue];
 	
+	NSLog(@"%s:%d %d->%d",__PRETTY_FUNCTION__,__LINE__, internal, external);
+
+	TCMPortMapping *newPortMapping = [TCMPortMapping portMappingWithPrivatePort:internal desiredPublicPort:external userInfo:nil];
+	[o_arrayController addObject:newPortMapping];
 	
+	[[TCMPortMapper sharedInstance] addPortMapping:newPortMapping];
+	
+	[o_sheetView orderOut:nil];
+	[NSApp endSheet:o_sheetView];
 }
 
 - (void)portMapperNotifications:(NSNotification *)aNotification {
