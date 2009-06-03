0a546d108e0a27da31331c1f9d86fb04395bd26d
Dominik Wagner <dom@codingmonkeys.de>
2009-06-03 11:16:32 +0000

    - removed performance inpact of attribute inspector if it is not visible

--- subethaedit/Debug/DebugAttributeInspectorController.m
+++ subethaedit/Debug/DebugAttributeInspectorController.m
@@ -25,41 +25,43 @@
 }
 
 - (void)textViewDidChangeSelection:(NSNotification *)aNotification {
-    NSTextView *textView = [aNotification object];
-    if ([[textView window] firstResponder] == textView && [[textView window] isMainWindow]) {
-    	NSArrayController *controller = nil;
-        NSTextStorage *textStorage = [textView textStorage];
-        NSTextStorage *firstTextStorage = textStorage;
-    	do {
-    		BOOL isFoldableTextStorage = [textStorage isKindOfClass:[FoldableTextStorage class]];
-    		controller = isFoldableTextStorage ? O_foldingTextStorageAttributesContentController : O_attributesContentController;
-			[controller setContent:[NSMutableArray array]];
-			NSRange selectedRange = [textView selectedRange];
-			if (!isFoldableTextStorage) selectedRange = [(id)firstTextStorage fullRangeForFoldedRange:selectedRange];
-			NSRange wholeRange = NSMakeRange(0,[textStorage length]);
-			if (selectedRange.location < NSMaxRange(wholeRange)) {
-				NSDictionary *attributes = [textStorage attributesAtIndex:selectedRange.location effectiveRange:NULL];
-				NSEnumerator *attributeEnumerator = [attributes keyEnumerator];
-				NSString *key=nil;
-				while ((key = [attributeEnumerator nextObject])) {
-					NSMutableDictionary *dictionary = [NSMutableDictionary dictionaryWithObject:key forKey:@"attributeName"];
-					NSString *value = [[attributes objectForKey:key] description];
-					if (value) {
-						[dictionary setObject:value forKey:@"contentValue"];
+	if ([[self window] isVisible]) {
+		NSTextView *textView = [aNotification object];
+		if ([[textView window] firstResponder] == textView && [[textView window] isMainWindow]) {
+			NSArrayController *controller = nil;
+			NSTextStorage *textStorage = [textView textStorage];
+			NSTextStorage *firstTextStorage = textStorage;
+			do {
+				BOOL isFoldableTextStorage = [textStorage isKindOfClass:[FoldableTextStorage class]];
+				controller = isFoldableTextStorage ? O_foldingTextStorageAttributesContentController : O_attributesContentController;
+				[controller setContent:[NSMutableArray array]];
+				NSRange selectedRange = [textView selectedRange];
+				if (!isFoldableTextStorage) selectedRange = [(id)firstTextStorage fullRangeForFoldedRange:selectedRange];
+				NSRange wholeRange = NSMakeRange(0,[textStorage length]);
+				if (selectedRange.location < NSMaxRange(wholeRange)) {
+					NSDictionary *attributes = [textStorage attributesAtIndex:selectedRange.location effectiveRange:NULL];
+					NSEnumerator *attributeEnumerator = [attributes keyEnumerator];
+					NSString *key=nil;
+					while ((key = [attributeEnumerator nextObject])) {
+						NSMutableDictionary *dictionary = [NSMutableDictionary dictionaryWithObject:key forKey:@"attributeName"];
+						NSString *value = [[attributes objectForKey:key] description];
+						if (value) {
+							[dictionary setObject:value forKey:@"contentValue"];
+						}
+						[controller addObject:dictionary];
 					}
-					[controller addObject:dictionary];
+					if (isFoldableTextStorage) {
+						NSMutableDictionary *bonusDictionary = [NSMutableDictionary dictionaryWithObject:@"Foldable Range" forKey:@"attributeName"];
+						// convert location to fulltextstorage before asking
+						selectedRange = [(id)textStorage fullRangeForFoldedRange:selectedRange];
+						[bonusDictionary setObject:NSStringFromRange([[(FoldableTextStorage *)textStorage fullTextStorage] foldableRangeForCharacterAtIndex:selectedRange.location]) forKey:@"contentValue"];
+						[controller addObject:bonusDictionary];
+					}
 				}
-				if (isFoldableTextStorage) {
-					NSMutableDictionary *bonusDictionary = [NSMutableDictionary dictionaryWithObject:@"Foldable Range" forKey:@"attributeName"];
-					// convert location to fulltextstorage before asking
-					selectedRange = [(id)textStorage fullRangeForFoldedRange:selectedRange];
-					[bonusDictionary setObject:NSStringFromRange([[(FoldableTextStorage *)textStorage fullTextStorage] foldableRangeForCharacterAtIndex:selectedRange.location]) forKey:@"contentValue"];
-					[controller addObject:bonusDictionary];
-				}
-			}
-			[controller rearrangeObjects];
-		} while ([textStorage respondsToSelector:@selector(fullTextStorage)] && (textStorage = [(id)textStorage fullTextStorage]));
-    }
+				[controller rearrangeObjects];
+			} while ([textStorage respondsToSelector:@selector(fullTextStorage)] && (textStorage = [(id)textStorage fullTextStorage]));
+		}
+	}
 }
 
 @end
