6fab221c0f4bdb1b54495f33856544a0123f16d4
Dominik Wagner <dom@codingmonkeys.de>
2009-11-02 17:54:59 +0000

    added the necessary menu items to the nib and context menu for the snow leopard features subtitution and transformation

--- subethaedit/AppController.m
+++ subethaedit/AppController.m
@@ -91,6 +91,8 @@ int const PasteMenuItemTag = 3;
 int const BlockeditMenuItemTag = 4;
 int const SpellingMenuItemTag = 10;
 int const SpeechMenuItemTag = 11;
+int const SubstitutionsMenuItemTag = 12;
+int const TransformationsMenuItemTag = 13;
 int const FormatMenuTag = 2000;
 int const FontMenuItemTag = 1;
 int const FileEncodingsMenuItemTag = 2001;
@@ -1034,6 +1036,8 @@ static OSStatus AuthorizationRightSetWithWorkaround(
     [defaultMenu addItem:[NSMenuItem separatorItem]];
     [defaultMenu addItem:[[(NSMenuItem *)[EditMenu itemWithTag:SpellingMenuItemTag] copy] autorelease]];
     [defaultMenu addItem:[[(NSMenuItem *)[FormatMenu itemWithTag:FontMenuItemTag] copy] autorelease]];
+    [defaultMenu addItem:[[(NSMenuItem *)[EditMenu itemWithTag:TransformationsMenuItemTag] copy] autorelease]];
+    [defaultMenu addItem:[[(NSMenuItem *)[EditMenu itemWithTag:SubstitutionsMenuItemTag] copy] autorelease]];
     [defaultMenu addItem:[[(NSMenuItem *)[EditMenu itemWithTag:SpeechMenuItemTag] copy] autorelease]];
     
     [TextView setDefaultMenu:defaultMenu];
