b7c5f37f711e463404a201dcb175b4c5ce58420f
Michael Ehrmann <michael@codingmonkeys.de>
2014-02-03 17:58:47 +0100

    [CHANGE] working auto layout code

--- SubEthaEdit-Mac/SEEParticipantsOverlayViewController.m
+++ SubEthaEdit-Mac/SEEParticipantsOverlayViewController.m
@@ -20,7 +20,9 @@
 
 @interface SEEParticipantsOverlayViewController ()
 @property (nonatomic, weak) PlainTextDocument *document;
-@property (nonatomic, strong) NSMutableArray *subviewControllers;
+@property (nonatomic, strong) NSMutableArray *participantSubviewControllers;
+@property (nonatomic, strong) NSMutableArray *inviteeSubviewControllers;
+@property (nonatomic, strong) NSMutableArray *pendingSubviewControllers;
 @end
 
 @implementation SEEParticipantsOverlayViewController
@@ -37,14 +39,15 @@
 		[NSNotificationCenter.defaultCenter addObserver:self selector:@selector(documentSessionDidChange:) name:TCMMMSessionPendingInvitationsDidChange object:documentSession];
 		[NSNotificationCenter.defaultCenter addObserver:self selector:@selector(documentSessionDidChange:) name:TCMMMSessionPendingUsersDidChangeNotification object:documentSession];
 
-		self.subviewControllers = [NSMutableArray array];
+		self.participantSubviewControllers = [NSMutableArray array];
+		self.inviteeSubviewControllers = [NSMutableArray array];
+		self.pendingSubviewControllers = [NSMutableArray array];
 	}
     return self;
 }
 
 
-- (void)dealloc
-{
+- (void)dealloc {
     [NSNotificationCenter.defaultCenter removeObserver:self];
 }
 
@@ -70,19 +73,22 @@
 	// cleanup old view hierachy
 	NSArray *subviews = [self.view.subviews copy];
 	[subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
-	[self.subviewControllers removeAllObjects];
+	[self.participantSubviewControllers removeAllObjects];
+	[self.inviteeSubviewControllers removeAllObjects];
+	[self.pendingSubviewControllers removeAllObjects];
 
 	// install new subviews for all allContributors
 	NSView *view = self.view;
 	TCMMMSession *session = self.document.session;
 	TCMMMUser *me = [TCMMMUserManager me];
 
+	// me
 	{
 		SEEParticipantViewController *participantViewController = [[SEEParticipantViewController alloc] initWithParticipant:me];
-		[self.subviewControllers addObject:participantViewController];
+		[self.participantSubviewControllers addObject:participantViewController];
 		[view addSubview:participantViewController.view];
 		NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:participantViewController.view
-																				attribute:NSLayoutAttributeLeading
+																				attribute:NSLayoutAttributeLeft
 																				relatedBy:NSLayoutRelationEqual
 																				   toItem:view
 																				attribute:NSLayoutAttributeLeft
@@ -100,6 +106,8 @@
 		[view addConstraints:@[horizontalConstraint, verticalConstraint]];
 		[participantViewController updateForParticipantUserState];
 	}
+
+	// Participants working on the document
 	{
 		NSMutableArray *allParticipants = [[session.participants objectForKey:TCMMMSessionReadWriteGroupName] mutableCopy];
 		[allParticipants addObjectsFromArray:[session.participants objectForKey:TCMMMSessionReadOnlyGroupName]];
@@ -108,8 +116,8 @@
 
 			SEEParticipantViewController *participantViewController = [[SEEParticipantViewController alloc] initWithParticipant:user];
 
-			NSView *lastUserView = [self.subviewControllers.lastObject view];
-			[self.subviewControllers addObject:participantViewController];
+			NSView *lastUserView = [self.participantSubviewControllers.lastObject view];
+			[self.participantSubviewControllers addObject:participantViewController];
 			[view addSubview:participantViewController.view];
 			NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:participantViewController.view
 																					attribute:NSLayoutAttributeLeft
@@ -131,6 +139,46 @@
 			[participantViewController updateForParticipantUserState];
 		}
 	}
+
+	NSView *spacerView = [[NSView alloc] init];
+	[spacerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+	[view addSubview:spacerView];
+	NSLayoutConstraint *verticalConstraint = [NSLayoutConstraint constraintWithItem:spacerView
+																		  attribute:NSLayoutAttributeTop
+																		  relatedBy:NSLayoutRelationEqual
+																			 toItem:view
+																		  attribute:NSLayoutAttributeTop
+																		 multiplier:1
+																		   constant:0];
+	NSLayoutConstraint *heightConstraint = [NSLayoutConstraint constraintWithItem:spacerView
+																		attribute:NSLayoutAttributeHeight
+																		relatedBy:NSLayoutRelationEqual
+																		   toItem:view
+																		attribute:NSLayoutAttributeHeight
+																	   multiplier:1
+																		 constant:0];
+
+	NSView *lastParticipantView = [[self.participantSubviewControllers lastObject] view];
+	if (lastParticipantView == nil) {
+		lastParticipantView = view;
+	}
+	NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:spacerView
+																			attribute:NSLayoutAttributeLeft
+																			relatedBy:NSLayoutRelationEqual
+																			   toItem:lastParticipantView
+																			attribute:NSLayoutAttributeRight
+																		   multiplier:1
+																			 constant:6];
+	NSLayoutConstraint *witdhConstraint = [NSLayoutConstraint constraintWithItem:spacerView
+																	   attribute:NSLayoutAttributeWidth
+																	   relatedBy:NSLayoutRelationGreaterThanOrEqual
+																		  toItem:nil
+																	   attribute:NSLayoutAttributeNotAnAttribute
+																	  multiplier:1
+																		constant:0];
+	[view addConstraints:@[verticalConstraint, heightConstraint, horizontalConstraint, witdhConstraint]];
+
+	// Invited user waiting to be accepted
 	{
 		NSMutableArray *allInvitees = [[session.invitedUsers objectForKey:TCMMMSessionReadWriteGroupName] mutableCopy];
 		[allInvitees addObjectsFromArray:[session.invitedUsers objectForKey:TCMMMSessionReadOnlyGroupName]];
@@ -141,8 +189,11 @@
 			if ([stateOfInvitee isEqualToString:TCMMMSessionInvitedUserStateAwaitingResponse]) {
 				SEEParticipantViewController *participantViewController = [[SEEParticipantViewController alloc] initWithParticipant:user];
 
-				NSView *lastUserView = [self.subviewControllers.lastObject view];
-				[self.subviewControllers addObject:participantViewController];
+				NSView *lastUserView = [self.inviteeSubviewControllers.lastObject view];
+				if (!lastUserView) {
+					lastUserView = spacerView;
+				}
+				[self.inviteeSubviewControllers addObject:participantViewController];
 				[view addSubview:participantViewController.view];
 				NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:participantViewController.view
 																						attribute:NSLayoutAttributeLeft
@@ -163,7 +214,7 @@
 				[view addConstraints:@[horizontalConstraint, verticalConstraint]];
 				[participantViewController updateForInvitationState];
 			} else {
-				// Move invited user to 
+				// Move invited user to
 				NSUserNotification *userNotification = [[NSUserNotification alloc] init];
 				userNotification.hasActionButton = NO;
 				userNotification.title = NSLocalizedString(@"User declined invitation.", @"User Notification title if a invited user declines your invitation.");
@@ -175,6 +226,46 @@
 			}
 		}
 	}
+
+	NSView *secondSpacerView = [[NSView alloc] init];
+	[secondSpacerView setTranslatesAutoresizingMaskIntoConstraints:NO];
+	[view addSubview:secondSpacerView];
+	verticalConstraint = [NSLayoutConstraint constraintWithItem:secondSpacerView
+													  attribute:NSLayoutAttributeTop
+													  relatedBy:NSLayoutRelationEqual
+														 toItem:view
+													  attribute:NSLayoutAttributeTop
+													 multiplier:1
+													   constant:0];
+	heightConstraint = [NSLayoutConstraint constraintWithItem:secondSpacerView
+													attribute:NSLayoutAttributeHeight
+													relatedBy:NSLayoutRelationEqual
+													   toItem:view
+													attribute:NSLayoutAttributeHeight
+												   multiplier:1
+													 constant:0];
+
+	NSView *lastInviteeView = [[self.inviteeSubviewControllers lastObject] view];
+	if (lastInviteeView == nil) {
+		lastInviteeView = spacerView;
+	}
+	horizontalConstraint = [NSLayoutConstraint constraintWithItem:secondSpacerView
+														attribute:NSLayoutAttributeLeft
+														relatedBy:NSLayoutRelationEqual
+														   toItem:lastInviteeView
+														attribute:NSLayoutAttributeRight
+													   multiplier:1
+														 constant:6];
+	witdhConstraint = [NSLayoutConstraint constraintWithItem:secondSpacerView
+												   attribute:NSLayoutAttributeWidth
+												   relatedBy:NSLayoutRelationEqual
+													  toItem:spacerView
+												   attribute:NSLayoutAttributeWidth
+												  multiplier:1
+													constant:0];
+	[view addConstraints:@[verticalConstraint, heightConstraint, horizontalConstraint, witdhConstraint]];
+
+	// Pending users to be accepted
 	{
 		NSArray *allPendingUsers = session.pendingUsers;
 		for (TCMMMUser *user in allPendingUsers) {
@@ -182,8 +273,11 @@
 
 			SEEParticipantViewController *participantViewController = [[SEEParticipantViewController alloc] initWithParticipant:user];
 
-			NSView *lastUserView = [self.subviewControllers.lastObject view];
-			[self.subviewControllers addObject:participantViewController];
+			NSView *lastUserView = [self.pendingSubviewControllers.lastObject view];
+			if (!lastUserView) {
+				lastUserView = secondSpacerView;
+			}
+			[self.pendingSubviewControllers addObject:participantViewController];
 			[view addSubview:participantViewController.view];
 			NSLayoutConstraint *horizontalConstraint = [NSLayoutConstraint constraintWithItem:participantViewController.view
 																					attribute:NSLayoutAttributeLeft
@@ -205,6 +299,18 @@
 			[participantViewController updateForPendingUserState];
 		}
 	}
+
+	// pin last subview to the right edge of its superview
+	NSView *lastSubview = [[view subviews] lastObject];
+	NSLayoutConstraint *pinToRightConstraint = [NSLayoutConstraint constraintWithItem:lastSubview
+																			attribute:NSLayoutAttributeRight
+																			relatedBy:NSLayoutRelationEqual
+																			   toItem:view
+																			attribute:NSLayoutAttributeRight
+																		   multiplier:1
+																			 constant:-6];
+	[view addConstraint:pinToRightConstraint];
+
 }
 
 @end
