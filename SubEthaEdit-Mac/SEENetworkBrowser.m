dec643fb00f7febad5eba4775adac6e08a66cb93
Michael Ehrmann <michael@codingmonkeys.de>
2014-02-18 15:08:54 +0100

    [ADD] add files for network browser

---
+++ SubEthaEdit-Mac/SEENetworkBrowser.m
@@ -0,0 +1,33 @@
+//
+//  SEENetworkBrowser.m
+//  SubEthaEdit
+//
+//  Created by Michael Ehrmann on 18.02.14.
+//  Copyright (c) 2014 TheCodingMonkeys. All rights reserved.
+//
+
+#import "SEENetworkBrowser.h"
+
+@interface SEENetworkBrowser ()
+
+@end
+
+@implementation SEENetworkBrowser
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
