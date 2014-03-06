5635fedacbc4e5f462948afe4cb18268005688f3
Michael Ehrmann <michael@codingmonkeys.de>
2014-03-06 16:37:53 +0100

    [ADD] special item object for toggling recent documents

---
+++ SubEthaEdit-Mac/SEEToggleRecentDocumentListItem.m
@@ -0,0 +1,13 @@
+//
+//  SEEToggleRecentDocumentListItem.m
+//  SubEthaEdit
+//
+//  Created by Michael Ehrmann on 06.03.14.
+//  Copyright (c) 2014 TheCodingMonkeys. All rights reserved.
+//
+
+#import "SEEToggleRecentDocumentListItem.h"
+
+@implementation SEEToggleRecentDocumentListItem
+
+@end
