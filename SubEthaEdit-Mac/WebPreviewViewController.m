12aaa5d80a382a0acbc03c71dbca2a735714a9bb
Michael Ehrmann <michael@codingmonkeys.de>
2014-03-20 13:29:52 +0100

    [ADD] adding a line :D

--- SubEthaEdit-Mac/WebPreviewViewController.m
+++ SubEthaEdit-Mac/WebPreviewViewController.m
@@ -403,6 +403,7 @@ NSScrollView * firstScrollView(NSView *aView) {
     }
 }
 
+
 - (void)webView:(WebView *)aSender didFinishLoadForFrame:(WebFrame *)aFrame {
     if ([aFrame isEqualTo:[self.oWebView mainFrame]]) {
         NSScrollView *scrollView=firstScrollView(self.oWebView);
