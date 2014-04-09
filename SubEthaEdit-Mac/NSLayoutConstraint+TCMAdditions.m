347368f5a1eb5f24d7028f18a8d6201bf3a8c114
Dominik Wagner <dom@codingmonkeys.de>
2014-04-09 15:00:29 +0200

    [ADD] added category for convenience constructors for constraints

---
+++ SubEthaEdit-Mac/NSLayoutConstraint+TCMAdditions.m
@@ -0,0 +1,20 @@
+//
+//  NSLayoutConstraint+TCMAdditions.m
+//  SubEthaEdit
+//
+//  Created by Dominik Wagner on 09.04.14.
+//  Copyright (c) 2014 TheCodingMonkeys. All rights reserved.
+//
+
+#import "NSLayoutConstraint+TCMAdditions.h"
+
+@implementation NSLayoutConstraint (TCMAdditions)
++ (instancetype)TCM_constraintWithItem:(id)aFirstItem secondItem:(id)aSecondItem equalAttribute:(NSLayoutAttribute)aLayoutAttribute {
+	NSLayoutConstraint *result = [NSLayoutConstraint constraintWithItem:aFirstItem attribute:aLayoutAttribute relatedBy:NSLayoutRelationEqual toItem:aSecondItem attribute:aLayoutAttribute multiplier:1.0 constant:0.0];
+	return result;
+}
++ (instancetype)TCM_constraintWithItem:(id)aFirstItem secondItem:(id)aSecondItem equalAttribute:(NSLayoutAttribute)aLayoutAttribute secondAttribute:(NSLayoutAttribute)aSecondLayoutAttribute {
+	NSLayoutConstraint *result = [NSLayoutConstraint constraintWithItem:aFirstItem attribute:aLayoutAttribute relatedBy:NSLayoutRelationEqual toItem:aSecondItem attribute:aSecondLayoutAttribute multiplier:1.0 constant:0.0];
+	return result;
+}
+@end
