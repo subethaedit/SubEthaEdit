8e9e74764daa68f05f48478ab5ff2662562fb6b9
Michael Ehrmann <michael@codingmonkeys.de>
2014-04-24 21:04:54 +0200

    [CHANGE] bind avatarView in DocumentList  instead of just setting the values...

--- SubEthaEdit-Mac/SEEDocumentListWindowController.m
+++ SubEthaEdit-Mac/SEEDocumentListWindowController.m
@@ -444,10 +444,15 @@ static void *SEENetworkDocumentBrowserEntriesObservingContext = (void *)&SEENetw
 		}
 
 		TCMMMUser *user = connectionRepresentation.user;
-		avatarView.image = user.image;
-		avatarView.initials = user.initials;
-		avatarView.borderColor = user.changeColorDesaturated;
-		avatarView.backgroundColor = user.changeHighlightColorWithWhiteBackground;
+		[avatarView bind:@"image" toObject:user withKeyPath:@"image" options:nil];
+		[avatarView bind:@"initials" toObject:user withKeyPath:@"initials" options:nil];
+		[avatarView bind:@"borderColor" toObject:user withKeyPath:@"changeColorDesaturated" options:nil];
+		[avatarView bind:@"backgroundColor" toObject:user withKeyPath:@"changeHighlightColorWithWhiteBackground" options:nil];
+
+//		avatarView.image = user.image;
+//		avatarView.initials = user.initials;
+//		avatarView.borderColor = user.changeColorDesaturated;
+//		avatarView.backgroundColor = user.changeHighlightColorWithWhiteBackground;
 	}
 }
 
