6c39cfa305439ac86e62d473d64b5efed304af94
Dominik Wagner <dom@codingmonkeys.de>
2014-05-08 13:35:01 +0200

    [CHANGE] further adjusted documentlist window (made avatars biggger, fonts to a little, adjusted layout of filenames)

--- SubEthaEdit-Mac/SEEDocumentListWindowController.m
+++ SubEthaEdit-Mac/SEEDocumentListWindowController.m
@@ -527,7 +527,7 @@ static void *SEENetworkDocumentBrowserEntriesObservingContext = (void *)&SEENetw
 	NSArray *availableDocumentSession = self.availableItems;
 	id documentRepresentation = [availableDocumentSession objectAtIndex:row];
 	if ([documentRepresentation isKindOfClass:SEENetworkConnectionDocumentListItem.class]) {
-		rowHeight = 46.0;
+		rowHeight = 50.0;
 	} else if ([documentRepresentation isKindOfClass:SEEToggleRecentDocumentListItem.class]) {
 		rowHeight = 28.0;
 	} else if ([documentRepresentation isKindOfClass:SEEConnectDocumentListItem.class]) {
