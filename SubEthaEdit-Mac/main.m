c9d1c6a3db5af5ce45fbaf5384b6afc047ec5605
Michael Ehrmann <michael@codingmonkeys.de>
2014-05-20 18:27:18 +0200

    [ADD] beta expire date for beta versions defined in XCConfig

--- SubEthaEdit-Mac/main.m
+++ SubEthaEdit-Mac/main.m
@@ -15,6 +15,27 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef BETA
+#ifdef BETA_EXPIRE_DATE
+
+#define STRINGIZE(x) #x
+#define STRINGIZE2(x) STRINGIZE(x)
+#define BETA_EXPIRE_DATE_LITERAL @ STRINGIZE2(BETA_EXPIRE_DATE)
+
+	NSString *expireDateString = BETA_EXPIRE_DATE_LITERAL;
+	NSDate *expireDate = [NSCalendarDate dateWithString:expireDateString];
+	NSDate *today = [NSDate date];
+	if ([today compare:expireDate] == NSOrderedDescending) {
+		NSLog(@"THIS BETA IS EXPIRED!");
+
+		NSAlert *alert = [NSAlert alertWithMessageText:@"This beta version of SubEthaEdit has expired." defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@"Please visit SubEthaEdit.de to download a new version.", nil];
+		[alert runModal];
+		return 0;
+	}
+
+#endif
+#endif
+
 #ifdef MAC_APP_STORE_RECEIPT_VALIDATION
 	return CheckReceiptAndRun(argc, argv);
 #else
