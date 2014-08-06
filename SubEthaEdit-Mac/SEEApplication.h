937ac08bde22b51ddc291c6da51dd25e0d3bcc5b
Michael Ehrmann <michael@codingmonkeys.de>
2014-08-06 17:27:21 +0200

    [ADD] SEEApplication, using custom NSApp subclass

---
+++ SubEthaEdit-Mac/SEEApplication.h
@@ -0,0 +1,13 @@
+//
+//  SEEApplication.h
+//  SubEthaEdit
+//
+//  Created by Michael Ehrmann on 06.08.14.
+//  Copyright (c) 2014 TheCodingMonkeys. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SEEApplication : NSApplication
+
+@end
---
+++ SubEthaEdit-Mac/SEEApplication.m
@@ -0,0 +1,34 @@
+//
+//  SEEApplication.m
+//  SubEthaEdit
+//
+//  Created by Michael Ehrmann on 06.08.14.
+//  Copyright (c) 2014 TheCodingMonkeys. All rights reserved.
+//
+
+#import "SEEApplication.h"
+#import "SEEDocumentController.h"
+#import "PlainTextDocument.h"
+
+@implementation SEEApplication
+
+- (IBAction)terminate:(id)sender {
+
+	NSArray *documents = [[SEEDocumentController sharedInstance] documents];
+
+	for (NSDocument *document in documents) {
+		if ([document isKindOfClass:[PlainTextDocument class]]) {
+			PlainTextDocument *plainTextDocument = (PlainTextDocument *)document;
+//			if ([plainTextDocument hasUnautosavedChanges]) {
+//				[plainTextDocument autosaveWithImplicitCancellability:NO completionHandler:^(NSError *errorOrNil) {
+//					
+//				}];
+//			}
+//			[plainTextDocument setPreparedForTermination:YES];
+		}
+	}
+
+	[super terminate:sender];
+}
+
+@end
