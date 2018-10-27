5f0b7b05e9f7688823512227220a9480a39b66ae
Dominik Wagner <dom@monkey.productions>
2018-10-27 16:04:01 +0200

    More types, adding custom icons for c, erlang and elixir, adding small header files.

--- SubEthaEdit-Mac/artwork/reduce.sh
+++ SubEthaEdit-Mac/artwork/reduce.sh
@@ -2,7 +2,6 @@
 SUBDIR=${1:-"."} # the nice defaulting bash operator https://stackoverflow.com/questions/6482377/check-existence-of-input-argument-in-a-bash-shell-script
 FLAGS="-unsharp 0x3+0.5+0"
 
-convert ${SUBDIR}/icon_512x512@2x.png -resize  16 $FLAGS ${SUBDIR}/icon_16x16.png
 convert ${SUBDIR}/icon_512x512@2x.png -resize  32 $FLAGS ${SUBDIR}/icon_32x32.png
 convert ${SUBDIR}/icon_512x512@2x.png -resize  64 $FLAGS ${SUBDIR}/icon_32x32@2x.png
 convert ${SUBDIR}/icon_512x512@2x.png -resize 128 $FLAGS ${SUBDIR}/icon_128x128.png
@@ -11,4 +10,11 @@ convert ${SUBDIR}/icon_512x512@2x.png -resize 512 $FLAGS ${SUBDIR}/icon_512x512.
 
 cp ${SUBDIR}/icon_512x512.png ${SUBDIR}/icon_256x256@2x.png
 cp ${SUBDIR}/icon_256x256.png ${SUBDIR}/icon_128x128@2x.png
-cp ${SUBDIR}/icon_32x32.png   ${SUBDIR}/icon_16x16@2x.png
+
+if [ ! -f ${SUBDIR}/icon_16x16@2x.png ]; then
+	convert ${SUBDIR}/icon_512x512@2x.png -resize  32 $FLAGS ${SUBDIR}/icon_16x16@2x.png
+fi
+
+convert ${SUBDIR}/icon_16x16@2x.png -resize  16 $FLAGS ${SUBDIR}/icon_16x16.png
+
+
