4eac041285088a9e0d04acb8a5106798208b4739
Dominik Wagner <dom@monkey.productions>
2020-12-03 11:02:31 +0100

    fixed warning, removed nslog and applied some code style changes

--- SubEthaEdit-Mac/Source/OpenURLViewController.h
+++ SubEthaEdit-Mac/Source/OpenURLViewController.h
@@ -1,17 +1,12 @@
-//
-//  SEEOpenURLView.h
+//  OpenURLViewController.h
 //  SubEthaEdit
 //
 //  Created by Jan Cornelissen on 29/11/2020.
-//  Copyright © 2020 SubEthaEdit Contributors. All rights reserved.
-//
 
 #import <Cocoa/Cocoa.h>
 
 @interface OpenURLViewController : NSViewController
+@property (nonatomic, strong) NSURL *url;
 
-@property (nonatomic, strong) NSURL *URLToOpen;
-
-- (instancetype)initWithURL:(NSURL *)anURLToOpen;
-
+- (instancetype)initWithURL:(NSURL *)url;
 @end
