e925caffd245e9d7a2e60d7a7c4fceae9577191c
Jan Cornelissen <jancornelissen@me.com>
2020-12-06 17:22:24 +0100

    Overhauled Edit preferences

---
+++ SubEthaEdit-Mac/Source/SEETertiaryLabelColorValueTransformer.h
@@ -0,0 +1,10 @@
+//  SEETertiaryLabelColorValueTransformer.h
+//  SubEthaEdit
+//
+//  Created by Jan Cornelissen on 06/12/2020.
+
+#import <Foundation/Foundation.h>
+
+@interface SEETertiaryLabelColorValueTransformer : NSValueTransformer
+
+@end
